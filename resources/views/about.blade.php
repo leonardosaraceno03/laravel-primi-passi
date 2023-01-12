bebaf6399225d60f965d0a289185a2b1bea168ce
leonardosaraceno03 <leonardosaraceno03@gmail.com>
2023-01-12 11:17:36 +0100

    finito

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -5,20 +5,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
+    <title>about</title>
 </head>
 
 <body>
-    <h1>Home page</h1>
-
-    <p>Hello {{ $name }} !</p>
-
-    <nav>
-        <ul>
-            <li><a href="/about">about</a></li>
-
-        </ul>
-    </nav>
+    <h1>about</h1>
 </body>
 
 </html>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    $name = "Leonardo";
+
+    return view('home', compact('name'));
+});
+Route::get('/about', function () {
+
+    return view('about');
 });
